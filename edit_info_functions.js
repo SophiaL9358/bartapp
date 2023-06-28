9f4b8f24f8234f4da9ee3dd7bda90077c2d503d4
soapity <mrows9356@gmail.com>
2023-06-27 21:35:37 -0400

    edit info done

--- edit_info_functions.js
+++ edit_info_functions.js
@@ -52,7 +52,50 @@ const getFirestoreValues = async () => {
 };
 
 let submitForm = async () => {
-    console.log('uwu');
+    // SETTING UP VARIABLES
+    // General info
+    let dry_food_cal_value = document.getElementById('dry_food_cal').value;
+    let wet_food_cal_value = document.getElementById('wet_food_cal').value;
+    let weight_lb_value = document.getElementById('weight_lb').value;
+    // Morning info
+    let mor_dry_value = document.getElementById('mor_dry_food_tbsp').value;
+    let mor_wet_value = document.getElementById('mor_wet_food_can').value;
+    // Afternoon info
+    let aft_dry_value = document.getElementById('aft_dry_food_tbsp').value;
+    let aft_wet_value = document.getElementById('aft_wet_food_can').value;
+
+
+    // UPDATING FIRESTORE
+    // update morning info
+    await updateDoc(doc(db, "Bart Info", "Morning"), {
+        dry_food_tbsp: mor_dry_value,
+        wet_food_can: mor_wet_value
+    })
+
+    // update afternoon info
+    await updateDoc(doc(db, "Bart Info", "Afternoon"), {
+        dry_food_tbsp: aft_dry_value,
+        wet_food_can: aft_wet_value
+    })
+
+    // update general info
+    // get given cal
+    let calc_given_cal = Number(dry_food_cal_value)/16*(Number(mor_dry_value)+Number(aft_dry_value)); // dry food
+    calc_given_cal += Number(wet_food_cal_value)/16*(Number(mor_wet_value)+Number(aft_wet_value)); // wet food
+    // rer = resting energy requirements
+    let rer = {6:149, 7:167, 8:184, 9:200, 10:218, 11:234, 12:250, 13:265, 14:280, 15:295, 16:310, 17:324, 18:339, 19:353, 20:366, 25:433};
+    await updateDoc(doc(db, "Bart Info", "General"), {
+        dry_food_cal: dry_food_cal_value,
+        wet_food_cal: wet_food_cal_value,
+        weight_lb: weight_lb_value,
+        rec_cal: rer[weight_lb_value],
+        given_cal: calc_given_cal
+    });
+
+    if (confirm("Is this good?\nRecommended Calories: "+rer[weight_lb_value] + "\nGiven Calories: "+calc_given_cal)){
+        window.location.href = 'home.html';
+    }
+
 };
 
 window.submitForm = submitForm;
